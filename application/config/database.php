cd797be404013a468345907714b1253b362d286b
Ari Susanto <risusanto@students.ilkom.unsri.ac.id>
2017-08-26 21:08:00 +0700

    change to mysqli

--- application/config/database.php
+++ application/config/database.php
@@ -80,7 +80,7 @@ $db['default'] = array(
 	'username' => $_ENV['DBUSER'],
 	'password' => $_ENV['DBPASS'],
 	'database' => $_ENV['DBNAME'],
-	'dbdriver' => 'mysql', //'mysqli',
+	'dbdriver' => 'mysqli', //'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
