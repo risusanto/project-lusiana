5e8eb13958e3b7fbdac4e4934676b4e90c7f4c89
Ari Susanto <risusanto@students.ilkom.unsri.ac.id>
2018-06-29 19:50:46 +0700

    Create db.php

---
+++ htdocs/db.php
@@ -0,0 +1,20 @@
+<?php
+$connectstr_dbhost = '';
+$connectstr_dbname = '';
+$connectstr_dbusername = '';
+$connectstr_dbpassword = '';
+
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+        continue;
+    }
+
+    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+    echo $connectstr_dbhost;
+    echo $connectstr_dbname;
+    echo $connectstr_dbusername;
+    echo $connectstr_dbpassword;
+}
